3c52f50762350e4d34fb89759cffecb9851c6fbf
blacksakura3c <250820552@qq.com>
2019-12-06 21:49:59 +0800

    修改了小组任务具体完成情况 上传了推送小组源代码信息

---
+++ Module/Subscription/ActivityCode/Check.java
@@ -0,0 +1,31 @@
+package com.example.whulife;
+
+import android.content.Intent;
+
+public class Check {
+    private String Num,Event,Details,Time,Place;
+    private MMessage temp;//MMessage是组员定义的更全面的部分 只是当时我的Check适配器用不了这么多 所以再做了次适配
+    public Check(MMessage parma){
+        this.Num=String.valueOf(parma.getMid());
+        this.Event=parma.getShortInfo();
+        this.Time=parma.getTime();
+        this.Place=parma.getLocation();
+        this.Details=parma.getInfo();
+    }
+
+    public String getNum(){
+        return this.Num;
+    }
+    public String getEvent(){
+        return this.Event;
+    }
+    public String getTime(){
+        return this.Time;
+    }
+    public String getPlace(){
+        return this.Place;
+    }
+    public String getDetails(){
+        return this.Details;
+    }
+}
---
+++ Module/Subscription/ActivityCode/CheckBoxAdapter.java
@@ -0,0 +1,110 @@
+package com.example.whulife;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.Map;
+
+public class CheckBoxAdapter extends RecyclerView.Adapter<CheckBoxAdapter.ViewHolder> {
+    private List<Check> myCheck;
+    public final HashMap<Integer,Boolean> map=new HashMap<>();
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        View checklistview;
+        CheckBox Selection;
+        TextView Num,Event,Time,Place;
+        public ViewHolder(View view){
+            super(view);
+            checklistview=view;
+            Selection=(CheckBox)view.findViewById(R.id.checkbox_checkboxlist_check);
+            Num=(TextView)view.findViewById(R.id.txtview_checkboxlist_num);
+            Event=(TextView)view.findViewById(R.id.textview_event);
+            Time=(TextView)view.findViewById(R.id.textview_time);
+            Place=(TextView)view.findViewById(R.id.textview_place);
+        }
+    }
+
+
+    public CheckBoxAdapter(List<Check> Checklist){
+        myCheck=Checklist;
+        //初始化时默认全为false
+        for(int i=0;i<myCheck.size();i++){
+            map.put(i,false);
+        }
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent,int viewtype){
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.checkboxlist_layout,parent,false);
+        final ViewHolder holder=new ViewHolder(view);
+        return holder;
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position){
+        Log.d("POSITION","position");
+        final Check check=myCheck.get(position);
+        holder.Num.setText(String.valueOf(position+1));
+        holder.Event.setText(check.getEvent());
+        holder.Event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog=new AlertDialog.Builder(v.getContext());
+                dialog.setTitle("详情");
+                dialog.setMessage(check.getDetails());
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("我知道啦", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show();
+            }
+        });
+        holder.Time.setText(check.getTime());
+        holder.Place.setText(check.getPlace());
+        holder.Selection.setChecked(map.get(position));
+        holder.Selection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("POSITION",String.valueOf(position));
+                map.put(position,!map.get(position));
+                notifyDataSetChanged();
+            }
+        });
+        //holder.Selection.setChecked(ischecked[position]);
+    }
+    @Override
+    public int getItemCount(){
+        return myCheck.size();
+    }
+
+    public void SelectAll(){
+        Set<Map.Entry<Integer, Boolean>> entries = map.entrySet();
+        for(Map.Entry<Integer,Boolean>entry:entries){
+            if(entry.getValue()==false)
+                entry.setValue(true);
+        }
+        notifyDataSetChanged();
+    }
+    public void ResetAll(){
+        Set<Map.Entry<Integer, Boolean>> entries = map.entrySet();
+        for(Map.Entry<Integer,Boolean>entry:entries){
+            if(entry.getValue()==true)
+                entry.setValue(false);
+        }
+        notifyDataSetChanged();
+    }
+
+}
