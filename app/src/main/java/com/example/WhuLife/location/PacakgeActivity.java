50d0d7d48a239719358760b6b0c149a6e6f67359
XJKunnn <49423850+XJKunnn@users.noreply.github.com>
2019-12-17 11:14:22 +0800

    引用数据库后的相关更改

--- app/src/main/java/com/example/WhuLife/location/PacakgeActivity.java
+++ app/src/main/java/com/example/WhuLife/location/PacakgeActivity.java
@@ -2,10 +2,16 @@ package com.example.WhuLife.location;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.amap.api.location.AMapLocationClient;
 import com.example.WhuLife.R;
@@ -20,6 +26,9 @@ public class PacakgeActivity extends AppCompatActivity implements View.OnClickLi
     public AMapLocationClient mapLocationClient = null;
     Location goal_l = new Location(0,0,"null");
 
+    private LocationDB dbHelper;
+    private SQLiteDatabase db;
+    private ContentValues values = new ContentValues();
     private List<APackage> packageList = new ArrayList<>();
     private PackageAdapter adapter;
     private int i;
@@ -30,24 +39,45 @@ public class PacakgeActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pacakge);
-
         fab = findViewById(R.id.add_button);
         fab.setOnClickListener(this);
 
-        //initPackage();
+        dbHelper = new LocationDB(this, "package.db", null, 2); //新创建时可能有问题
+        dbHelper.getWritableDatabase();
+        db = dbHelper.getWritableDatabase();
+
+
         adapter = new PackageAdapter(this, R.layout.package_item, packageList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
-
+        initPackage();
 //        mapLocationClient = new AMapLocationClient((this.getApplicationContext()));
 //        myLocation.initLocation(mapLocationClient);
 //        myLocation.setLocation(30, 114);
         //myLocation.startLocation();
     }
 
-//    private void initPackage(){
-//
-//    }
+    private void initPackage(){
+        Cursor cursor = db.query("package", null,null,null,null,null, null);
+        if(cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(cursor.getColumnIndex("id"));
+                String packageLocation = cursor.getString(cursor.getColumnIndex("package_location"));
+                String packageCompany = cursor.getString(cursor.getColumnIndex("package_company"));
+                String informTime = cursor.getString(cursor.getColumnIndex("inform_time"));
+                APackage add_one = new APackage(id+"", packageLocation, "null", packageCompany);
+                adapter.notifyDataSetChanged();
+                packageList.add(add_one);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 
     @Override
     protected void onDestroy(){
@@ -62,7 +92,6 @@ public class PacakgeActivity extends AppCompatActivity implements View.OnClickLi
                 view.setFocusableInTouchMode(true);
                 Intent intent = new Intent(this, setLocation.class);
                 startActivityForResult(intent, 1);
-
                 break;
         }
     }
@@ -73,14 +102,26 @@ public class PacakgeActivity extends AppCompatActivity implements View.OnClickLi
         switch (requestCode) {
             case 1:
                 if(resultCode == RESULT_OK){
-                    goal_l.setfetchLocation(data.getStringExtra("location"));
-                    goal_l.setLatitude(data.getDoubleExtra("latitude", 0));
-                    goal_l.setLongitude(data.getDoubleExtra("longtitude", 0));
-                    goal_l.setCompany(data.getStringExtra("company"));
+                    String fetchLocation = data.getStringExtra("location");
+                    String company = data.getStringExtra("company");
+                    double latitude = data.getDoubleExtra("latitude", 0);
+                    double longtitude = data.getDoubleExtra("longtitude", 0);
                     String packageId = data.getStringExtra("packageId");
-                    APackage add_one = new APackage(packageId, goal_l.getfetchLocation(), "0", goal_l.getCompany());
+
+                    APackage add_one = new APackage(packageId, fetchLocation, "null", company);
                     adapter.notifyDataSetChanged();
                     packageList.add(add_one);
+
+                    //data
+                    values.put("id", packageId);
+                    values.put("package_location", fetchLocation);
+                    values.put("package_company", company);
+                    values.put("latitude", latitude);
+                    values.put("longtitude", longtitude);
+                    values.put("inform_time", "null");
+                    //insert
+                    db.insert("package", null, values);
+                    values.clear();
                 }
                 break;
             default:
