8ef45e1793f7f1a2511fdb853371bca4f429523e
ukuq <ukuq@qq.com>
2019-12-19 23:56:38 +0800

    webview pushnewsactivity添加loadbar

--- app/src/main/java/com/example/WhuLife/subscription/PushNewsActivity.java
+++ app/src/main/java/com/example/WhuLife/subscription/PushNewsActivity.java
@@ -1,10 +1,14 @@
 package com.example.WhuLife.subscription;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,16 +16,33 @@ import com.example.WhuLife.R;
 
 public class PushNewsActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pushnews_layout);
-        WebView webView=(WebView)findViewById(R.id.webview);
+        WebView webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        Intent intent=getIntent();
-        String url=intent.getStringExtra("URL");
-        Log.d("URL",url);
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("URL");
+        Log.d("URL", url);
         webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(PushNewsActivity.this, "加载完毕", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar = findViewById(R.id.process_bar);
+                progressBar.setVisibility(View.VISIBLE);
+                Toast.makeText(PushNewsActivity.this, "加载中...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
